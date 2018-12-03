13b3300508c8f91c86f034f8439559e1f62fae39
MrRopke <Theisropke@gmail.com>
2018-12-03 10:48:57 +0100

    ffyy

--- WaterMasterAPI/Controllers/SensorController.cs
+++ WaterMasterAPI/Controllers/SensorController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SensorModels;
 using System.Data.SqlClient;
+using WaterMasterAPI.Controllers;
 
 namespace WaterMasterAPI.Controllers
 {
@@ -14,29 +15,8 @@ namespace WaterMasterAPI.Controllers
     public class SensorController : ControllerBase
     {
         private string connString = "Server=tcp:cybonspace.database.windows.net,1433;Initial Catalog=MyDB;Persist Security Info=False;User ID=cybon;Password=Password1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        
-        // POST: api/Sensor 
-        // Refactored over in SensorDataController
+                
         [HttpPost]
-        public void PostSensorData([FromBody] SensorData sensorData)
-        {
-            using (SqlConnection conn = new SqlConnection(connString))
-            {
-                conn.Open();
-
-                string sqlString = "INSERT INTO SensorData VALUES(@Humidity, @Date, @FK_MacAddress)";
-                SqlCommand cmd = new SqlCommand(sqlString, conn);
-
-                cmd.Parameters.AddWithValue("@Humidity", sensorData.Humidity);
-                cmd.Parameters.AddWithValue("@Date", sensorData.Date);
-                cmd.Parameters.AddWithValue("@FK_MacAddress", sensorData.FK_MacAddress);
-
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        [HttpPost]
-        [Route("PostSensor/")]
         public void PostSensor([FromBody] Sensor sensor)
         {
             using (SqlConnection conn = new SqlConnection(connString))
@@ -56,7 +36,6 @@ namespace WaterMasterAPI.Controllers
             }
         }
 
-
         [HttpPut]
         public void UpdateSensor([FromBody] Sensor sensor)
         {
@@ -73,43 +52,15 @@ namespace WaterMasterAPI.Controllers
 
                 cmd.ExecuteNonQuery();
             }
-        }
-
-        // Refactored over in SensorDataController
-        [HttpGet]
-        public SensorData GetSensorData(string MacAddress)
-        {
-            SensorData sd = null;
-
-            using (SqlConnection conn = new SqlConnection(connString))
-            {
-                conn.Open();
-
-                string sqlString = "SELECT TOP 1 * FROM SensorData WHERE SensorData.FK_MacAddress = '" + MacAddress + "' ORDER BY SensorData.Id DESC";
-                SqlCommand cmd = new SqlCommand(sqlString, conn);
-
-                SqlDataReader reader = cmd.ExecuteReader();
-                
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        sd = new SensorData { FK_MacAddress = reader[3].ToString(), Id = Convert.ToInt32(reader[0]), Humidity = Convert.ToDouble(reader[1]), Date = Convert.ToDateTime(reader[2]) };                       
-                    }
-                }
-
-                reader.Close();
-            }
-
-            return sd;
-        }
+        }        
 
         // GET: api/Sensor/5
         [HttpGet]
         [Route("Mac/{MacAddress}")]
         public Sensor GetSensor(string MacAddress)
         {
-            SensorData sd = GetSensorData(MacAddress);
+            SensorDataController sdc = new SensorDataController();
+            SensorData sd = sdc.GetSensorData(MacAddress);
             Sensor sensor = null;
 
             using (SqlConnection conn = new SqlConnection(connString))
@@ -161,40 +112,5 @@ namespace WaterMasterAPI.Controllers
             }
             return macAddress;
         }
-
-
-
-
-        //// GET: api/Sensor
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET: api/Sensor/5
-        //[HttpGet("{id}", Name = "Get")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST: api/Sensor
-        //[HttpPost]
-        //public void Post([FromBody] string value)
-        //{
-        //}
-
-        //// PUT: api/Sensor/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //// DELETE: api/ApiWithActions/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
