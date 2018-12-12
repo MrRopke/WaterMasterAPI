b3aecb02980af6ddbebf80dd30a733630adadff3
Michael-MO <Michael-MO@users.noreply.github.com>
2018-12-12 13:02:10 +0100

    Add Cors

--- WaterMasterAPI/Startup.cs
+++ WaterMasterAPI/Startup.cs
@@ -24,6 +24,7 @@ namespace WaterMasterAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,6 +35,11 @@ namespace WaterMasterAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(
+                options =>
+                options.AllowAnyMethod().AllowAnyOrigin()
+                );
 
             app.UseMvc();
         }
